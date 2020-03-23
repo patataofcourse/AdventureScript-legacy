962e378c910bf56ab183925285bf619d039be98f
patataofcourse <jmvieitez9@gmail.com>
2020-03-23 22:58:25 +0100

    Add an actual readme

---
+++ Readme.md
@@ -0,0 +1,15 @@
+# Commands right now:
+- [n] - wait for input
+- [goto] - goes to a specific line, can pass extra arguments
+- [flag] - sets flags, not implemented yet
+- [choice] - prompts player with choice between options and a way to save
+- [loadscript] - changes script
+- [ending] - ends the script managing and returns
+# To be added:
+- Variable support
+- List support
+- Conditionals and checking variables/lists/flags
+- A title screen support, a way to save
+- Exceptions instead of err() [partial support, will be added to]
+- Doing something that's not printing to return the text, wait, etc
+- Use OOP for the commands and arguments [in progress]
--- adventurescript/__init__.py
+++ adventurescript/__init__.py
@@ -1,21 +1,3 @@
-'''
-Commands right now:
-  [n] - wait for input
-  [goto] - goes to a specific line, can pass extra arguments
-  [flag] - sets flags, not implemented yet
-  [choice] - prompts player with choice between options and a way to save
-  [loadscript] - changes script
-  [ending] - ends the script managing and returns
-To be added:
- -Variable support
- -List support
- -Conditionals and checking variables/lists/flags
- -A title screen support, a way to save
- -Exceptions instead of err() [partial support]
- -Doing something that's not printing to return the text, wait, etc
- -Use OOP for the commands and arguments
-'''
-
 from commands import commands
 import os
 import platform
