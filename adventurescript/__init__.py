5908a0f516e6b1debf9e711fae46be24e5542602
patataofcourse <jmvieitez9@gmail.com>
2020-06-16 17:24:50 +0200

    Create inventory class

--- adventurescript/__init__.py
+++ adventurescript/__init__.py
@@ -4,6 +4,57 @@ from adventurescript.info import ContextInfo
 import os
 import platform
 
+class Inventory:
+    def __init__(self, size, start_items={}, money=0):
+        if size == 0:
+            raise Exception("Inventory size cannot be 0") #TODO
+        if len(start_items) > size:
+            raise Exception("Inventory size too small for all the starting items given!") #TODO
+        self.inv = [None]*size
+        for item in start_items:
+            self.add(item, start_items[item])
+        self.money = money
+    def find(self, item, min_quantity=1):
+        for elmt in inventory:
+            if item == elmt[0]:
+                return inventory.index(elmt) if min_quantity <= elmt[1] else None
+        return None
+    def upgrade(self, plus_size):
+        self.inv += [None] * plus_size
+    def downgrade(self, minus_size):
+        if len(self.inv) - minus_size == 0:
+            raise Exception("Inventory size cannot be 0") #TODO
+        elif self.inv.count(None) < minus_size:
+            raise Exception("Trying to remove inventory size, but there's still some items in the portion to be removed") #TODO
+        else:
+            self.inv = self.inv[:len(self.inv)-minus_size]
+    def add(self, item, quantity=1):
+        item_in_inv = False
+        for pair in self.inv:
+            if pair != None and item == pair[0]:
+                item_in_inv = True
+                pos = self.inv.index(pair)
+        if item_in_inv:
+            self.inv[pos][1] += quantity
+        else:
+            try:
+                empty_slot = self.inv.index(None)
+            except ValueError:
+                return False
+            else:
+                self.inv[empty_slot] = [item, quantity]
+                return True
+    def remove(self, item, quantity=1):
+        pos = self.find(item, quantity)
+        if pos == None:
+            self.inv[pos][1] -= quantity
+            if self.inv[pos][1] == 0:
+                self.inv.pop(pos)
+                self.inv.append(None)
+    def represent(self):
+        out = ""
+        return out
+
 async def parse(name, save_id=0, show=defaultio.show, wait=defaultio.wait, query=defaultio.query, pass_info = False, addons = [], is_async=False):
     info = ContextInfo(name, save_id, show, wait, query, is_async, pass_info)
     
