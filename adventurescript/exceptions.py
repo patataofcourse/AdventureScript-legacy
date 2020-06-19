cc291729b0ae505734a3dc1a75b0119f65159047
patataofcourse <jmvieitez9@gmail.com>
2020-06-19 20:45:59 +0200

    Exception cleanup, woo!

--- adventurescript/exceptions.py
+++ adventurescript/exceptions.py
@@ -7,21 +7,21 @@ class CommandException(Exception):
         self.args = (f"Error in script '{scriptname}'.adv, line {line}, in command {cmdname}: {info}",)
 
 class UndefinedObjectError(Exception):
-    def __init__(self, scriptname, line, name, cmdname=None):
-        self.args = (f"'{varname}' doesn't exist! ({scriptname}.adv, line {line}{(', command' + cmdname) if cmdname != None else ''})",)
+    def __init__(self, scriptname, line, name):
+        self.args = (f"'{varname}' doesn't exist! ({scriptname}.adv, line {line})",)
 
 class UndefinedVariableError(UndefinedObjectError):
-    def __init__(self, scriptname, line, varname, cmdname=None):
-        self.args = (f"Variable '{varname}' doesn't exist! ({scriptname}.adv, line {line}{(', command' + cmdname) if cmdname != None else ''})",)
+    def __init__(self, scriptname, line, varname):
+        self.args = (f"Variable '{varname}' doesn't exist! ({scriptname}.adv, line {line})",)
 
 class UndefinedLabelError(UndefinedObjectError):
-    def __init__(self, scriptname, line, lblname, cmdname=None):
-        self.args = (f"Label '{chr(123)+lblname+chr(125)}' doesn't exist! ({scriptname}.adv, line {line}{(', command' + cmdname) if cmdname != None else ''}",)
+    def __init__(self, scriptname, line, lblname):
+        self.args = (f"Label '{chr(123)+lblname+chr(125)}' doesn't exist! ({scriptname}.adv, line {line}",)
 
 class UndefinedFlagError(UndefinedObjectError):
-    def __init__(self, scriptname, line, flagname, cmdname=None):
-        self.args = (f"Flag '{flagname}' doesn't exist! ({scriptname}.adv, line {line}{(', command' + cmdname) if cmdname != None else ''})",)
+    def __init__(self, scriptname, line, flagname):
+        self.args = (f"Flag '{flagname}' doesn't exist! ({scriptname}.adv, line {line})",)
     
 class UndefinedListError(UndefinedObjectError):
-    def __init__(self, scriptname, line, listname, cmdname=None):
-        self.args = (f"List '{listname}' doesn't exist! ({scriptname}.adv, line {line}{(', command' + cmdname) if cmdname != None else ''})",)
+    def __init__(self, scriptname, line, listname):
+        self.args = (f"List '{listname}' doesn't exist! ({scriptname}.adv, line {line})",)
